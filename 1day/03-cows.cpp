6d36df7915142bee2f9556a5e57b430b57785cdb
Polina Chubenko <chubenko.pn@phystech.edu>
2023-08-02 01:09:15 +0300

    updates

--- 1day/03-cows.cpp
+++ 1day/03-cows.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
+#include <vector>
 
-// vector<int> coord
-// k 
-
-bool check(int x) {
+bool check(int x, int k, std::vector<int> &coords) {
     int cows_cnt = 1;
     int last_cow = coords[0];
-    // for (int i = 0; i < coord.size(); ++i) {
-    //     coord[i]
-    // }
     for (int c : coords) {
         if (c - last_cow >= x) {
             ++cows_cnt;
@@ -19,11 +14,20 @@ bool check(int x) {
 }
 
 int main () {
+    int n;
+    std::cin >> n;
+    std::vector<int> coords(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> coords[i];
+    }
+    int k;
+    std::cin >> k;
+
     int left = 0;
-    int right = coord.back() - coord[0]; // coord[coord.size() - 1] = coord.back()
+    int right = coords.back() - coords[0] + 1; // coord[coord.size() - 1] = coord.back()
     while (right - left > 1) {
         int middle = (left + right) / 2;
-        if (check(middle)) {
+        if (check(middle, k, coords)) {
             left = middle;
         } else {
             right = middle;
--- 1day/05-modify.cpp
+++ 1day/05-modify.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-
-int main () {
-    // v, k
-    int cnt;
-    int j = 1;
-    for (int i = 0; i < n; ++i) {
-        while (j != n - 1 && v[j] - v[i] <= k) {
-            ++j;
-        }
-        cnt += n - j;
+#include <vector>
+ 
+int main() {
+    int x;
+    std::vector<int> a;
+    while (std::cin >> x) {  // читаем числа, пока не закончится ввод
+        a.push_back(x);  // добавляем очередное число в вектор
     }
-    std::cout << cnt;
+ 
+    while (!a.empty() && a.back() == 0) {
+        // Пока вектор не пуст и последний элемент равен нулю
+        a.pop_back();  // удаляем этот нулевой элемент
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        std::cout << a[i] << " ";
+    }
+
 }
